77457b9782e7ed6457b3b667e713e1d8abc9f5bc
aminaummabdulla <tminopt@gmail.com>
2020-10-10 15:27:07 +0200

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-arrow-left slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-arrow-right slick-prev"></button>',
+        speed: 2000
 
+
+  });
 
 });
